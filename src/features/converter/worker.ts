e6f5f2d0b15d7cbcc1a54bd5fd90734ef8da8269
julian-saleix <julian.saleix@gmail.com>
2024-10-15 17:03:51 +0200

    chore: quoting worker.ts content

--- src/features/converter/worker.ts
+++ src/features/converter/worker.ts
@@ -1,21 +1,21 @@
-import { WorkerEvent } from "../../types/worker";
-import { convertWebPToJPGBase64 } from "../../utils/file";
+// import { WorkerEvent } from "../../types/worker";
+// import { convertWebPToJPGBase64 } from "../../utils/file";
 
-onmessage = async function (event: WorkerEvent) {
-    console.log("Received message from the main thread:", event.data);
-    const { type, payload } = event.data;
+// onmessage = async function (event: WorkerEvent) {
+//     console.log("Received message from the main thread:", event.data);
+//     const { type, payload } = event.data;
 
-    switch (type) {
-        case "convert":
-            console.log("here");
-            const res = await convertWebPToJPG(payload);
-            postMessage(res);
-            break;
-        default:
-            console.error(`Unhandled event type ${type}`);
-    }
-};
+//     switch (type) {
+//         case "convert":
+//             console.log("here");
+//             const res = await convertWebPToJPG(payload);
+//             postMessage(res);
+//             break;
+//         default:
+//             console.error(`Unhandled event type ${type}`);
+//     }
+// };
 
-const convertWebPToJPG = async (files: File[]) => {
-    return Promise.all(files.map(convertWebPToJPGBase64));
-};
+// const convertWebPToJPG = async (files: File[]) => {
+//     return Promise.all(files.map(convertWebPToJPGBase64));
+// };
